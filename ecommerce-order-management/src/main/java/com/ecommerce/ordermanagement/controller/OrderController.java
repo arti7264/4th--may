1209568069bb7055bcc2018e18c4518cc47cf504
Arti Amle <artibamle2000@gmail.com>
2026-08-11 23:58:37 +0530

    Add ecommerce order management application

---
+++ ecommerce-order-management/src/main/java/com/ecommerce/ordermanagement/controller/OrderController.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ordermanagement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+
+    @GetMapping("/orders")
+    public String getOrders() {
+        return "Order Management API is working!";
+    }
+}
